c0dd3df37aa734c4335498b0a41a0d8a066306d2
Ben White <bwhite2140@outlook.com>
2020-06-04 14:39:22 -0700

    fix issues with incorrect information displaying
    Co-authored-by: Deryck Jackson <Dgjackson01@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -22,32 +22,40 @@ const scalene = function(side1, side2, side3) {
   }
 }
 
+const notTriangle = function(side1, side2, side3) {
+  if (side1 + side2 <= side3 || side1 + side3 <= side2 || side2 + side3 <= side1){
+    return true;
+  } else {
+    return false;
+  }
+}
+
 $(document).ready(function() {
   $("#form").submit(function(event) {
     event.preventDefault();
     const side1 = parseInt($("#side1").val());
     const side2 = parseInt($("#side2").val());
     const side3 = parseInt($("#side3").val());
-    if (equilateral(side1, side2, side3)) {
-      $(".hidden#equilateral").show();
-      $(".hidden#isosceles").hide();
-      $(".hidden#scalene").hide();
-      $(".hidden#notTriangle").hide();
-    } else if (isosceles) {
-      $(".hidden#equilateral").hide();
-      $(".hidden#isosceles").show();
-      $(".hidden#scalene").hide();
-      $(".hidden#notTriangle").hide();
-    } else if (scalene) {
-      $(".hidden#equilateral").hide();
-      $(".hidden#isosceles").hide();
-      $(".hidden#scalene").show();
-      $(".hidden#notTriangle").hide();
-    } else {
+    if (notTriangle(side1, side2, side3)) {
       $(".hidden#equilateral").hide();
       $(".hidden#isosceles").hide();
       $(".hidden#scalene").hide();
       $(".hidden#notTriangle").show();
+    } else if (equilateral(side1, side2, side3)) {
+      $(".hidden#equilateral").show();
+      $(".hidden#isosceles").hide();
+      $(".hidden#scalene").hide();
+      $(".hidden#notTriangle").hide();
+    } else if (isosceles(side1, side2, side3)) {
+      $(".hidden#equilateral").hide();
+      $(".hidden#isosceles").show();
+      $(".hidden#scalene").hide();
+      $(".hidden#notTriangle").hide();
+    } else {
+      $(".hidden#equilateral").hide();
+      $(".hidden#isosceles").hide();
+      $(".hidden#scalene").show();
+      $(".hidden#notTriangle").hide();
     }
   });
 });
